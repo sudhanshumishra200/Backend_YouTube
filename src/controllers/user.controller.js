d6f23182662f02bac695e92d840765ac83f8f52c
sudhanshu <mishrasudhanshu2020@gmail.com>
2024-07-27 13:40:05 +0530

    update the conteoller and asynchandler

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,11 +1,18 @@
 import asyncHandler from '../utils/asyncHandler.js'
+import User from '../models/User.model.js'
 
 
-
+// we want to register the user 
 const registerUser = asyncHandler(async (req,res) => {
-    res.status(200).json({
-        message: 'User registered successfully'
-    })
+    //get user details from frontend
+    //validation - not empty
+    // check if user already eexist: username and email
+    //check for images, check for avatar
+    //upload them to cloudinart, avatar 
+    //create user object - create entry in db 
+    // remove password and refresh token field from response
+    //check for user creation 
+    //return response
 })
 
 export {registerUser}
